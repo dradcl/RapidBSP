44985084eea0042040c3394ad511daa0b91b39a3
Karma Kitten <53679096+karmakittenx@users.noreply.github.com>
2020-11-04 00:55:26 -0500

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System.IO;
+﻿using RapidBSP.Lumps;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Forms;
-using System.Windows.Input;
 
 namespace RapidBSP
 {
@@ -11,11 +14,20 @@ namespace RapidBSP
     /// </summary>
     public partial class MainWindow : Window
     {
+        int lumpIndex = 0, selectedIndex = 0;
+        VBSPFile vBSPFile;
+        string[] lumpNames = Enum.GetNames(typeof(LumpTypeIndex2004));
         public MainWindow()
         {
             InitializeComponent();
         }
-
+        private void UpdateLumpInformation(int index1, int index2)
+        {
+            byteSize.Content = vBSPFile.lumps[index1].filelen;
+            offsetLbl.Content = vBSPFile.lumps[index1].fileofs;
+            versionLbl.Content = vBSPFile.lumps[index1].version;
+            lumpType.Content = lumpNames[index2];
+        }
         private void menuOpen_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
@@ -25,34 +37,48 @@ namespace RapidBSP
                 VBSPFile file = new VBSPFile(ofd.FileName);
             }
         }
-
         private void menuDirOpen_Click(object sender, RoutedEventArgs e)
         {
             FolderBrowserDialog fbd = new FolderBrowserDialog();
 
             if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
             {
-                string[] files = Directory.GetFiles(fbd.SelectedPath);
+                List<string> files = Directory.GetFiles(fbd.SelectedPath).ToList();
+
+                foreach (string file in files.ToList())
+                {
+                    if (!file.EndsWith(".bsp"))
+                    {
+                        files.Remove(file);
+                    }
+                }
 
                 fileList.ItemsSource = files;
-                fileCount.Content = files.Length + " files: ";
+                fileCount.Content = files.Count + " files: ";
             }
         }
-
-        //  https://stackoverflow.com/questions/6938752/wpf-how-do-i-handle-a-click-on-a-listbox-item
-        private void fileList_OnPreviewMouseDown(object sender, MouseButtonEventArgs e)
+        private void lumpsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            var item = ItemsControl.ContainerFromElement(fileList, e.OriginalSource as DependencyObject) as ListBoxItem;
-            if (item != null)
+            lumpIndex = lumpsList.Items.IndexOf(lumpsList.SelectedItem);
+            
+            if (lumpIndex > -1 && lumpIndex < 64)
             {
-                VBSPFile vBSPFile = new VBSPFile((string) item.Content);
-
-                validBSP.Content = vBSPFile.IsValidBSPFile();
-                byteSize.Content = vBSPFile.lumps[2].filelen;
-                offsetLbl.Content = vBSPFile.lumps[2].fileofs;
-                versionLbl.Content = vBSPFile.lumps[2].version;
-                lumpsList.ItemsSource = vBSPFile.lumps;
+                selectedIndex = lumpsList.SelectedIndex;
+                UpdateLumpInformation(selectedIndex, lumpIndex);
             }
+            else
+            {
+                lumpIndex = 0;
+            }
+        }
+        private void fileList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            vBSPFile = new VBSPFile(fileList.SelectedItem.ToString());
+            lumpsList.ItemsSource = vBSPFile.lumps;
+            lumpsList.SelectedIndex = selectedIndex;
+
+            validBSP.Content = vBSPFile.IsValidBSPFile();
+            UpdateLumpInformation(selectedIndex, selectedIndex);
         }
     }
 }
